b219503657208469d2cc767f5bd521a7cffa4b68
Crowdbotics <team@crowdbotics.com>
2020-10-15 16:44:09 +0000

    Update components

--- src/features/BlankScreen311456/screens/blank.js
+++ src/features/BlankScreen311456/screens/blank.js
@@ -1,46 +1,40 @@
 import React from "react"
-import { View, Image, ImageBackground } from "react-native"
 import {
-  withStyles,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
   Text,
   Button,
-  CheckBox,
-  Radio,
-  Toggle,
-  Icon,
-  Input,
-  Datepicker
-} from "react-native-ui-kitten"
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
 import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
-export class _Blank extends React.Component {
+export class Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
   render = () => (
-    <View style={styles.View_1}>
-      <Button
-        title="Press me!"
-        color="#3366FF"
-        textStyle={{
-          fontSize: 12,
-          color: "#000000",
-          textAlign: "center",
-          fontWeight: "normal",
-          fontStyle: "normal"
-        }}
-        style={styles.Button_2}
-        onPress={() => alert("Pressed!")}
-      />
+    <View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Text style={styles.Text_4}>Sample text content</Text>
     </View>
   )
 }
 
-Blank = withStyles(_Blank, theme => ({
-  container: { backgroundColor: theme["color-basic-100"] }
-}))
+const styles = StyleSheet.create({
+  container: { flex: 1, marginHorizontal: 16 },
+  View_1: {},
+  Button_2: {},
+  Text_4: { color: "#df0707" }
+})
 
 function mapStateToProps(state) {
   return { state: state.apiReducer }
